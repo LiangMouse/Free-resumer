e15668a97100ad0dedd3d59fc3040e4700d02865
shualiang <shualiang@ebay.com>
2025-03-19 10:43:50 +0800

    init twice

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
